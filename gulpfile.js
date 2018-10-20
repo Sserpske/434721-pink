2c267b70a3f71800918e5971b4f9c0202d39d719
Александр Кочура <kochura@gmail.com>
2018-10-20 19:52:51 +0300

    webp и svg-sprite

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,11 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var svgSprite = require('gulp-svg-sprite');
+var svgmin = require('gulp-svgmin');
+var cheerio = require('gulp-cheerio');
+var replace = require('gulp-replace');
+var webp = require('gulp-webp');
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -32,3 +37,36 @@ gulp.task("server", function () {
 });
 
 gulp.task("start", gulp.series("css", "server"));
+
+
+
+gulp.task('svg-sprite', function () {
+  return gulp.src('source/img/*.svg')
+  // minify svg
+    .pipe(svgmin({
+      js2svg: {
+        pretty: true
+      }
+    }))
+    // build svg sprite
+    .pipe(svgSprite({
+      mode: {
+        symbol: {
+          sprite: "source/img/sprite.svg",
+          render: {
+            scss: {
+              dest:'source/sass/sprite1.scss',
+              template: "source/sass/template.scss"
+            }
+          }
+        }
+      }
+    }))
+    .pipe(gulp.dest('source/img'));
+});
+
+gulp.task('webp', () =>
+    gulp.src("source/img/*.{png,jpg}")
+        .pipe(webp({quality: 80}))
+        .pipe(gulp.dest('source/img/test'))
+);
